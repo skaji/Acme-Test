13cc3616d7e659f33d7e63073930a83aede3d445
Shoichi Kaji <skaji@cpan.org>
2018-03-04 11:26:53 +0900

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+[![Build Status](https://travis-ci.org/skaji/Acme-Test.svg?branch=master)](https://travis-ci.org/skaji/Acme-Test)
+
+# NAME
+
+Acme::Test - Blah blah blah
+
+# SYNOPSIS
+
+    use Acme::Test;
+
+# DESCRIPTION
+
+Acme::Test is
+
+# AUTHOR
+
+Shoichi Kaji <skaji@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+Copyright 2018 Shoichi Kaji <skaji@cpan.org>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
---
+++ lib/Acme/Test.pm
@@ -0,0 +1,35 @@
+package Acme::Test;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Test - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Acme::Test;
+
+=head1 DESCRIPTION
+
+Acme::Test is
+
+=head1 AUTHOR
+
+Shoichi Kaji <skaji@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2018 Shoichi Kaji <skaji@cpan.org>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
